3ede2a50cec3e12aee1469d03b4fcf6810993f88
Axviral <Onitredy2@gmail.com>
2026-02-16 22:48:12 +0600

    update

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import path from "path";
+import { fileURLToPath } from "url";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import { viteSingleFile } from "vite-plugin-singlefile";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), viteSingleFile()],
+  base: './', // Ensure relative paths for static hosting
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    outDir: 'dist',
+    target: 'esnext',
+  }
+});
